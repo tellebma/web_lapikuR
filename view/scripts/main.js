83fb4723bbd3abe00d5f8b62bb1d7ad0be5f8dfe
eriau <mathieu.eriau@hotmail.fr>
2021-09-08 14:31:39 +0200

    Ajout recherche pathologie via textbox

--- view/scripts/main.js
+++ view/scripts/main.js
@@ -1,4 +1,4 @@
 function fillHeader(){
-    document.getElementsByClassName("nav1")[0].innerHTML = '<div class="nav-wrapper"><a href="#" class="brand-logo">NavBar</a><ul id="nav-mobile" class="right hide-on-med-and-down"><li><a href="sass.html">Sass</a></li><li><a href="badges.html">Components</a></li><li><a href="collapsible.html">JavaScript</a></li></ul></div>';
+    document.getElementsByClassName("nav")[0].innerHTML = '<div class="nav-wrapper container"><a href="#" class="brand-logo">LaPikuR</a><ul id="nav-mobile" class="right hide-on-med-and-down"><li><a href="sass.html">Sass</a></li><li><a href="badges.html">Components</a></li><li><a href="collapsible.html">JavaScript</a></li></ul></div>';
 }
 

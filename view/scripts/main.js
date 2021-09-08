89972e54d9f1246097674e904b26d623ee606e79
eriau <mathieu.eriau@hotmail.fr>
2021-09-08 14:58:51 +0200

    Tentative de turcs sur criteres.html c'est la cata

--- view/scripts/main.js
+++ view/scripts/main.js
@@ -1,4 +1,7 @@
+$(document).ready(function() {
+    $('select').material_select();
+    });
+
 function fillHeader(){
     document.getElementsByClassName("nav")[0].innerHTML = '<div class="nav-wrapper container"><a href="#" class="brand-logo">LaPikuR</a><ul id="nav-mobile" class="right hide-on-med-and-down"><li><a href="sass.html">Sass</a></li><li><a href="badges.html">Components</a></li><li><a href="collapsible.html">JavaScript</a></li></ul></div>';
 }
-

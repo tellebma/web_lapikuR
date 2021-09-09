af7c2b9d20a91b703dfe4ed3c2372c6fe292b5f7
eriau <mathieu.eriau@hotmail.fr>
2021-09-09 21:28:35 +0200

    C'EST SACREMENT DE LA MERDE PUTIN

---
+++ services/dataBase.php
@@ -0,0 +1,15 @@
+<?php
+
+class DataBase {
+    function __construct() {
+        // Constructeur
+    }
+
+    function conect() {
+
+    }
+
+    // Bref, J EN PEUX PLUS DE CETTE VM DE FILS DE PUTE QUI MET DES MAJS POUR RIEN
+} 
+
+?>

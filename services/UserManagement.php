0651fbd6d009502c224c4c9e1bba4817f9832381
eriau <mathieu.eriau@hotmail.fr>
2021-09-12 23:51:01 +0200

    Avancement UserManagement.php

--- services/UserManagement.php
+++ services/UserManagement.php
@@ -4,9 +4,9 @@ require 'services/DataBase.php';
 
 class UserManagement {
     function login($ident, $pass){
-        $result = ((new DataBase())->connect())->query("SELECT * FROM `user` WHERE `name` LIKE '".$ident."' ;");
-        while ($row = $result->fetch()){
-            print_r($row); // C'est comme ça pour seek des data
+        $result = ((new DataBase())->connect())->query("SELECT * FROM `user` WHERE `name` LIKE '".$ident."' AND `pass` LIKE '".$pass."';");
+        while ($row = $result->fetch()){ // C'est comme ça pour parse les results
+            print_r($row); // En gros maintenant faut check si y a un résultat, et s'il y en a un ba on lui donne un token ou une merde comme ça
         }
     }
 } 

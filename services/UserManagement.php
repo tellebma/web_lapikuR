032b469b933ce38199ffacc3f550a8cb640bc463
tellebma <66943979+tellebma@users.noreply.github.com>
2021-09-13 16:19:22 +0200

    test UserManagement.php
    a verifier !

--- services/UserManagement.php
+++ services/UserManagement.php
@@ -6,8 +6,30 @@ class UserManagement {
     function login($ident, $pass){
         $result = ((new DataBase())->connect())->query("SELECT * FROM `user` WHERE `name` LIKE '".$ident."' AND `pass` LIKE '".$pass."';");
         while ($row = $result->fetch()){ // C'est comme ça pour parse les results
-            print_r($row); // En gros maintenant faut check si y a un résultat, et s'il y en a un ba on lui donne un token ou une merde comme ça
+            //$hash = base64_encode($ident.$pass)
+            sessionStorage.setItem('user_session', base64_encode($ident.$pass));
+            //print_r($row); // En gros maintenant faut check si y a un résultat, et s'il y en a un ba on lui donne un token ou une merde comme ça
         }
+    }
+    function register($id,$pass,$email){
+        $result = ((new DataBase())->connect())->query("SELECT * FROM `user_db` WHERE `user` LIKE '".$id."';");
+        $find = FALSE;
+        foreach  ($result as $row) {
+            if ($row["user"] == $id){
+              $find = TRUE;
+              break;
+            }
+          }
+          if ($find == TRUE){
+            print_r("Vous devez trouver un autre nom d'utilisateur !");
+          }else {
+              $result = ((new DataBase())->connect())->query("INSERT INTO `user_db` (`id`, `user`, `password` , `email`) VALUES (NULL, '".$id."', '".$pass."', '".$email."');");
+            if ($result === TRUE) {
+                echo "Vous pouvez maintenant vous connecter");
+            } else {
+                print_r("Error: " . $q . "<br>" . $conn->error);
+            }
+    }
     }
 } 
 
